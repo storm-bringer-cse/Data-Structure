0a904dd1fdc4f89a39c0b91ac174243bcfe0d198
ridwan_ahmed <ridwanahmed264@gmail.com>
2025-04-23 15:31:22 +0600

    Add files via upload

---
+++ print_i.c
@@ -0,0 +1,16 @@
+//print an array
+#include<stdio.h>
+int main() {
+   int n;
+   scanf("%d",&n);
+   int a[n];
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+   for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+   }
+
+
+    return 0;
+}
