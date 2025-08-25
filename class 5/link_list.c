87ff240cc5fde6e20ed570a49032bbf856479c81
ridwan_ahmed <ridwanahmed264@gmail.com>
2025-08-25 21:57:23 +0600

    Update link_list.c

--- class 5/link_list.c
+++ class 5/link_list.c
@@ -2,31 +2,32 @@
 #include<stdlib.h>
 
 struct node {
-    int data ;
+    int data;
     struct node* next;
 };
+
 int main(){
     struct node* head =(struct node*) malloc(sizeof(struct node));
     struct node* second = (struct node*)malloc(sizeof(struct node));
     struct node* third = (struct node*)malloc(sizeof(struct node));
+
     head -> data =10;
     head -> next = second;
+
     second -> data =20;
     second -> next = third;
+
     third -> data = 30;
     third -> next = NULL;
-    struct node* current = head;
-    while(current -> next!=NULL) {
-        current = current -> next;
 
-        current = head;
-        while(current!= NULL){
-            printf("%d->",current -> data);
-            current = current -> next;
-        }
-        printf("NULL!");
-        free(head);
-        free(second);
-        free(third);
+    struct node* current = head;
+    while(current != NULL) {
+        printf("%d -> ", current -> data);
+        current = current -> next;
     }
+    printf("NULL\n");
+
+    free(head);
+    free(second);
+    free(third);
 }
