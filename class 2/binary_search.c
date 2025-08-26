bd270478cb18688aa7b2e762890375c2a5648267
ridwan_ahmed <ridwanahmed264@gmail.com>
2025-08-26 08:48:38 +0600

    Update binary_search.c

--- class 2/binary_search.c
+++ class 2/binary_search.c
@@ -1,40 +1,48 @@
 #include<stdio.h>
-int binarysearch(int arr[],int size,int target){
-int beg=0,mid,end=size-1;
-while(beg>=end){
-    mid=(beg - end)/2;
-    if(arr[mid]==target){
-      return mid;
+
+int binarysearch(int ar[], int size, int target) {
+    int beg = 0, end = size - 1, mid;
+    while (beg <= end) {
+        mid = beg + (end - beg) / 2;
+
+        if (ar[mid] == target) {
+            return mid;
+        }
+        else if (ar[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            beg = mid + 1;
+        }
     }
-    else if(arr[mid]>target){
-        end = mid -1;
-    }
-    else if(arr[mid]<target){
-        beg = mid + 1;
+    return -1;
+}
+
+int main() {
+    int ar[100], size, target, result;
+    printf("Enter size: ");
+    scanf("%d", &size);
+
+  
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &ar[i]);
     }
 
-}
-return -1;
-}
-int main(){
-    int arr[100],i,result,size,target;
-    printf("Enter size :");
-    scanf("%d",&size);
-    for(int i=0;i<size;i++){
-        scanf("%d",&arr[i]);
-    }
-    printf(" Enter target :");
-    scanf("%d",&target);
-    result = binarysearch(arr,size,target);
-    if(result != -1){
-        printf("Item found");
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    result = binarysearch(ar, size, target);
+
+    if (result != -1) {
+        printf("Item found" );
     }
     else {
-    printf("Item not  Found");
+        printf("Item not found!");
     }
     return 0;
-
 }
+
+
 
 
 
